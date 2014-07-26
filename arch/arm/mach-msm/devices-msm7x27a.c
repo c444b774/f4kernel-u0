c2b13fb7f466ab70f12fb43eaac2a7300c445b3c
Fauzan <f4uzan@f4-pc.f4>
2014-07-26 17:49:01 +0700

    re-tweaked some values

--- arch/arm/mach-msm/devices-msm7x27a.c
+++ arch/arm/mach-msm/devices-msm7x27a.c
@@ -220,7 +220,7 @@ struct platform_device msm_device_dmov = {
 };
 
 static struct acpuclk_pdata msm7x27a_acpuclk_pdata = {
-	.max_speed_delta_khz = 400000,
+	.max_speed_delta_khz = 450000,
 };
 
 struct platform_device msm7x27a_device_acpuclk = {
@@ -899,6 +899,10 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 		/* LGE_CHANGE_S [peter.jung@lge.com]
 		    DoU Power consumption */
 		{
+			.gpu_freq = 380000000,
+			.bus_freq = 245760000,
+		},
+		{
 			.gpu_freq = 355760000,
 			.bus_freq = 213760000,
 		},
@@ -914,15 +918,11 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 			.gpu_freq = 192000000,
 			.bus_freq = 160000000,
 		},
-		{
-			.gpu_freq = 133330000,
-			.bus_freq = 0,
-		},
 	},
 	.init_level = 0,
 	/* LGE_CHANGE_S [peter.jung@lge.com]
 	     DoU Power consumption */
-	.num_levels = 4,
+	.num_levels = 5,
 	/* LGE_CHANGE_S [peter.jung@lge.com] */
 	.set_grp_async = set_grp_xbar_async,
 	.idle_timeout = HZ,
