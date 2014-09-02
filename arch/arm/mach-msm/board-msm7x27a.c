0dcda239aa25304ab1a13f0e4029cca2b1e9ec45
F4uzan <derp.indonesia@gmail.com>
2014-09-02 21:49:15 +0700

    board: 7627a: tweaked residency rate

--- arch/arm/mach-msm/board-msm7x27a.c
+++ arch/arm/mach-msm/board-msm7x27a.c
@@ -404,7 +404,7 @@ static struct msm_pm_platform_data msm8625_pm_data[MSM_PM_SLEEP_MODE_NR * 2] = {
 					.idle_enabled = 0,
 					.suspend_enabled = 0,
 					.latency = 500,
-					.residency = 6000,
+					.residency = 500,
 	},
 
 	[MSM_PM_MODE(0, MSM_PM_SLEEP_MODE_WAIT_FOR_INTERRUPT)] = {
@@ -423,7 +423,7 @@ static struct msm_pm_platform_data msm8625_pm_data[MSM_PM_SLEEP_MODE_NR * 2] = {
 					.idle_enabled = 0,
 					.suspend_enabled = 0,
 					.latency = 500,
-					.residency = 6000,
+					.residency = 500,
 	},
 
 	[MSM_PM_MODE(1, MSM_PM_SLEEP_MODE_WAIT_FOR_INTERRUPT)] = {
